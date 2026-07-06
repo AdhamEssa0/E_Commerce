bd7783197afd508935ac9abe198ab9ff687cb88a
AdhamEssa0 <hhod0044@gmail.com>
2026-07-06 18:05:16 +0300

    Unit Work

---
+++ E_Commerce.Doman/Contracts/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using E_Commerce.Doman.Commen;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Doman.Contracts
+{
+    public interface IUnitOfWork
+    {
+        Task<int> SaveChangesAsync(CancellationToken ct =  default);
+        IGenericRepository<TEntity , TKey> GetRepository<TEntity, TKey>() where TEntity : BaseEntity<TKey>;
+    }
+}
---
+++ E_Commerce.Infrastracture/Repository/UnitOfWork.cs
@@ -0,0 +1,31 @@
+﻿using E_Commerce.Doman.Commen;
+using E_Commerce.Doman.Contracts;
+using E_Commerce.Infrastracture.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Infrastracture.Repository
+{
+    public class UnitOfWork(StoreDbContext dbContext) : IUnitOfWork
+    {
+        private readonly Dictionary<string, object> repositories = [];
+        public IGenericRepository<TEntity, TKey> GetRepository<TEntity, TKey>() where TEntity : BaseEntity<TKey>
+        {
+            var TypeName = typeof(TEntity).Name;
+            if (repositories.TryGetValue(TypeName, out object? value))
+                return (IGenericRepository<TEntity, TKey>) value;
+            else
+            {
+                var repo = new GenericRepository<TEntity, TKey>(dbContext);
+                repositories[TypeName] = repo;
+                return repo;
+            }
+        }
+
+        public async Task<int> SaveChangesAsync(CancellationToken ct = default)
+            => await dbContext.SaveChangesAsync(ct);
+    }
+}
