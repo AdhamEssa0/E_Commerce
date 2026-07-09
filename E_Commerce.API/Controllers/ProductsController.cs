1ce16d4475b4496be6df4e1a204003d396298bcf
AdhamEssa0 <hhod0044@gmail.com>
2026-07-09 15:38:07 +0300

    Emhance Response

--- E_Commerce.API/Controllers/ProductsController.cs
+++ E_Commerce.API/Controllers/ProductsController.cs
@@ -2,13 +2,12 @@
 using E_Commerce.Application.Contracts;
 using E_Commerce.Application.DTOs.ProductDtos;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 
 namespace E_Commerce.API.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class ProductsController : ControllerBase
+    public class ProductsController : ApiBaseController
     {
         private readonly IProductService _productService;
 
@@ -19,36 +18,39 @@ namespace E_Commerce.API.Controllers
         // Get All Product 
         // GET :: BaseUrl/api/Product
         [HttpGet]
-        public async Task<ActionResult<Result<IReadOnlyList<ProductDto>>>> GetAllProducts(CancellationToken ct = default!)
+        public async Task<ActionResult<IReadOnlyList<ProductDto>>> GetAllProducts(CancellationToken ct = default!)
         {
             var result = await _productService.GetAllProductsAsync(ct);
-            return Ok(result);
+            return ToActionResult(result);
+
         }
 
         // GET :: Product by id
         // GET :: BaseUrl/api/Product/{Id}
         [HttpGet("{id}")]
-        public async Task<ActionResult<Result<ProductDto>>> GetProduct(int id , CancellationToken ct)
+        public async Task<ActionResult<ProductDto>> GetProduct(int id , CancellationToken ct)
         {
             var result = await _productService.GetProductsAsync(id, ct);
-            return Ok(result);
+            return ToActionResult(result);
         }
         // GET All brands
         // GET :: BaseUrl/api/Product/brands
         [HttpGet("brands")]
-        public async Task<ActionResult<Result<IReadOnlyList<BrandDto>>>> GetAllBrands(CancellationToken ct = default!)
+        public async Task<ActionResult<IReadOnlyList<BrandDto>>> GetAllBrands(CancellationToken ct = default!)
         {
             var brands = await _productService.GetAllBrandsAsync(ct);
-            return Ok(brands);
-        } 
-        
+            return ToActionResult(brands);
+
+        }
+
         // GET All types
         // GET :: BaseUrl/api/Product/types
         [HttpGet("types")]
-        public async Task<ActionResult<Result<IReadOnlyList<TypeDto>>>> GetAllTypes(CancellationToken ct = default!)
+        public async Task<ActionResult<IReadOnlyList<TypeDto>>> GetAllTypes(CancellationToken ct = default!)
         {
             var types = await _productService.GetAllTypeAsync(ct);
-            return Ok(types);
+            return ToActionResult(types);
+
         }
 
     }
